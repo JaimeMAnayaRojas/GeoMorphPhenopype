ea01643ce4592aa1178b11b2a3e1476930d89c8d
JaimeMAnayaRojas <jmanaya30@gmail.com>
2022-11-15 15:56:03 +0100

    update script

--- GeoMorph.py
+++ GeoMorph.py
@@ -10,37 +10,57 @@ import phenopype as pp
 import os
 
 ## change working dir - will contain project folder
-os.chdir(r"/home/jaime/sciebo/GeoMorph/_temp/")
+os.chdir(r"/home/jaime/sciebo/GeoMorphPhenopype/_temp")
 
 ## set project name
 project_name = "project_2"
 
 ## fetch template from downloaded template repo (https://github.com/phenopype/phenopype-templates)
-template_path = r"/home/jaime/sciebo/GeoMorph/landmarks_ref.yaml"
+template_path = r"/home/jaime/sciebo/GeoMorphPhenopype/detection_mask_ref.yaml"
 
 ## set directory with images  
-image_dir = r"/home/jaime/sciebo/GeoMorph/data/photos"
+image_dir = r"/home/jaime/sciebo/GeoMorphPhenopype/data/photos"
 
 
 proj = pp.Project(project_name)
 
-
+os.listdir(image_dir)
 ## add all stickleback-images from the data folder, but exclude the two that don't belong to the series 
-proj.add_files(image_dir = image_dir)
+proj.add_files(image_dir = image_dir, include="IMG", exclude=["side","top"])
 
 ## add the config template; provide a tag
 proj.add_config(template_path=template_path, tag="v1", overwrite=True)
 
+
+
+## modify the "preprocessing" section
+
+target1 = """    - preprocessing:"""
+replacement1 = """    - preprocessing:
+        - add_reference"""
+
+proj.edit_config(
+    tag="v1",
+    target=target1,
+    replacement=replacement1
+)
+
+
+replacement2 = """    - preprocessing:
+        - detect_reference"""
+
+proj.edit_config(
+    tag="v1",
+    target=target1,
+    replacement=replacement2
+)
+
+
+
 ## set the project-wide reference. the reference has its own tag, in case your project uses multiple reference cards
-## How should I do it when each picture are likely to have a different reference
-proj.dir_paths[0]
+# proj.add_reference(reference_image_path= os.path.join(image_dir,"stickleback_side.jpg"), reference_tag="stickle-scale")
+# how to add reference for all?
 
-fileList = os.listdir(image_dir)
-#proj.add_reference(reference_image_path= os.path.join(image_dir,"stickleback_side.jpg"), reference_tag="stickle-scale")
-proj.add_reference(reference_image_path= os.path.join(image_dir,fileList[0]), reference_tag="stickle-scale")
-
-
-# I am not able to continue to the next picture or to change the settings
 ## run image processing
 for path in proj.dir_paths:
     pp.Pype(path, tag="v1")
